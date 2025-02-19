bf5467fef6bd2265eba029bf3e4cb95c82d72c6e
NatCoding-devTeam <mlysiasnathan@gmail.com>
2025-02-19 15:37:00 +0200

    Task done : init project HomeSweeter

--- components/Home/AboutSection.js
+++ components/Home/AboutSection.js
@@ -5,7 +5,7 @@ const AboutSection = ({ data, locale }) => {
   return (
     <section className={classes.main}>
       <h3>
-        { locale == 'ar-DZ' ? 'اربين هوم' : 'Arben Home' }
+        { locale == 'ar-DZ' ? 'اربين هوم' : 'Home SWEETER' }
       </h3>
       <p>
         { locale == 'ar-DZ' ? data.ar : locale == 'fr-FR' ? data.fr : data.en }
