e4beeb6f44ccd2934a0ec5eb8606474710a1e203
NatCoding-devTeam <mlysiasnathan@gmail.com>
2025-02-22 00:57:44 +0200

    Task done: working on BOOKing system

--- src/app/rentals/[id]/page.jsx
+++ src/app/rentals/[id]/page.jsx
@@ -1,13 +1,17 @@
 "use client";
 import React, {useEffect, useState} from "react";
-import {useParams, useSearchParams} from "next/navigation";
+import {useParams, useRouter, useSearchParams} from "next/navigation";
 import {IoBookmark, IoHome, IoLocation} from "react-icons/io5";
 import {toast} from "react-toastify";
 import {mutate} from "swr";
 import {useSession} from "next-auth/react";
 import TitleText from "../../../components/TitleText";
 
+
+// https://youtu.be/h13mMp6-81Y
+
 const PropertyPage = () => {
+    const router = useRouter();
     const [booking, setBooking] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const {data: session} = useSession();
@@ -47,10 +51,13 @@ const PropertyPage = () => {
 
     }
 
+
     const searchParams = useSearchParams();
     const {id} = useParams(); // Get ID from the URL
     const [error, setError] = useState(null);
+    const [isInit, setIsInit] = useState(false);
     const [user, setUser] = useState(null);
+    const [existingBooking, setExistingBooking] = useState({});
     const [property, setProperty] = useState(
         searchParams.get("state") ? JSON.parse(searchParams.get("state")) : null
     );
@@ -63,11 +70,18 @@ const PropertyPage = () => {
                 .catch((err) => setError(err));
         }
     }, [id, property]);
-    if (property) {
+    if (property && !isInit) {
         fetch(`/api/users/${property.hostId}`)
             .then((res) => res.json())
             .then((data) => setUser(data))
-            .catch((err) => setError(err));
+            .catch((err) => setError(err))
+            .then((_) =>
+                fetch(`/api/booking/${property.id}`)
+                    .then((res) => res.json())
+                    .then((data) => setExistingBooking(data))
+                    .catch((err) => setError(err))
+                    .then((_) => setIsInit(true)))
+        ;
     }
     if (error || property == null) return <div className="text-red-600">{error}</div>;
     if (!property) return <TitleText title="Loading property..."/>;
@@ -88,7 +102,10 @@ const PropertyPage = () => {
                                     : "bg-slate-200 text-slate-500"
                             } h-12 text-md w-48 px-7 rounded-full mt-3`}
                             disabled={isLoading}
-                            onClick={addBooking}>
+                            onClick={() => {
+                                toast.success("Deleting Done");
+                                router.back()
+                            }}>
                             {isLoading ? "Deleting..." : "Delete"}
                         </button>
                         <button
@@ -98,7 +115,7 @@ const PropertyPage = () => {
                                     : "bg-slate-200 text-slate-500"
                             } h-12 text-md w-48 px-7 rounded-full mt-3 mx-3`}
                             disabled={isLoading}
-                            onClick={addBooking}>
+                            onClick={() => toast.success("Feature available in next update")}>
                             {isLoading ? "Editing..." : "Edit"}
                         </button>
                     </>
@@ -140,38 +157,45 @@ const PropertyPage = () => {
                     <div className="flex flex-col gap-2">
                         <h5 className="text-2xl text-gray-900 font-bold">Booking</h5>
                         <p className="mb-4 text-base text-gray-500 border-l-4 border-third-color pl-4">
-
-                            <p>Check In Date</p>
-                            <input
-                                className='md:w-[350px] w-full h-12 rounded-full mt-4 mr-10 bg-slate-200 text-third-color px-3 focus:outline-none border-third-color focus:border-4 border-2'
-                                type='date'
-                                placeholder='Check in Date'
-                                onChange={(e) => {
-                                    booking.checkIn = e.target.value
-                                    booking.checkIn.trim()
-                                    setBooking({...booking})
-                                }}/>
-                            <hr/>
-                            <p>Check Out Date</p>
-                            <input
-                                className='md:w-[350px] w-full h-12 rounded-full mt-4 mr-10 bg-slate-200 text-third-color px-3 focus:outline-none border-third-color focus:border-4 border-2'
-                                type='date'
-                                placeholder='Check out Date'
-                                onChange={(e) => {
-                                    booking.checkOut = e.target.value
-                                    booking.checkOut.trim()
-                                    setBooking({...booking})
-                                }}/>
-                            <button
-                                className={`${
-                                    !isLoading
-                                        ? "bg-third-color text-white"
-                                        : "bg-slate-200 text-slate-500"
-                                } h-12 text-md w-48 px-7 rounded-full mt-3`}
-                                disabled={isLoading}
-                                onClick={addBooking}>
-                                {isLoading ? "Adding..." : "Book"}
-                            </button>
+                            {"id" in existingBooking ?
+                                <>
+                                    <p className="text-red-600">{existingBooking.status}</p>
+                                </>
+                                :
+                                <>
+                                    <p>Check In Date</p>
+                                    <input
+                                        className='md:w-[350px] w-full h-12 rounded-full mt-4 mr-10 bg-slate-200 text-third-color px-3 focus:outline-none border-third-color focus:border-4 border-2'
+                                        type='date'
+                                        placeholder='Check in Date'
+                                        onChange={(e) => {
+                                            booking.checkIn = e.target.value
+                                            booking.checkIn.trim()
+                                            setBooking({...booking})
+                                        }}/>
+                                    <hr/>
+                                    <p>Check Out Date</p>
+                                    <input
+                                        className='md:w-[350px] w-full h-12 rounded-full mt-4 mr-10 bg-slate-200 text-third-color px-3 focus:outline-none border-third-color focus:border-4 border-2'
+                                        type='date'
+                                        placeholder='Check out Date'
+                                        onChange={(e) => {
+                                            booking.checkOut = e.target.value
+                                            booking.checkOut.trim()
+                                            setBooking({...booking})
+                                        }}/>
+                                    <button
+                                        className={`${
+                                            !isLoading
+                                                ? "bg-third-color text-white"
+                                                : "bg-slate-200 text-slate-500"
+                                        } h-12 text-md w-48 px-7 rounded-full mt-3`}
+                                        disabled={isLoading}
+                                        onClick={addBooking}>
+                                        {isLoading ? "Adding..." : "Book"}
+                                    </button>
+                                </>
+                            }
                         </p>
                     </div>
                 </div>
